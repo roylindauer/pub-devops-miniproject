2d3f5dee2746c0f51125eab3bcae8f9157c9174d
Alex L <alex@engagedmd.com>
2023-02-27 12:20:51 -0500

    mini project initial

--- bin/infra.ts
+++ bin/infra.ts
@@ -1,21 +1,96 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as path from 'path';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stack = new cdk.Stack(app, 'EMD-FargateService15',
+ // { env: { account: AWS_ACCOUNT, region: AWS_REGION }}
+);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// Create VPC
+const vpc = new ec2.Vpc(stack, 'VPC', {
+  maxAzs: 2,
+  natGateways: 0,
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// Create Load Balancer
+const alb = new elbv2.ApplicationLoadBalancer(stack, 'EMD-ApplicationLoadBalancer', {
+  vpc: vpc,
+  internetFacing: true,
+  ipAddressType: elbv2.IpAddressType.IPV4,
+});
+
+// Create Fargate Cluster
+const cluster = new ecs.Cluster(stack, 'Cluster', { vpc });
+
+// Create ECS Task Definition Template
+const fargateTaskDefinition = new ecs.FargateTaskDefinition(stack, `EMD-FargateTaskDefinition`, {
+  family: `EMD-CDK-fargateTaskDefinition`,
+  cpu: 512,
+  memoryLimitMiB: 1024,
+});
+
+// Create AWS Fargate Container
+const fargateContainer = new ecs.ContainerDefinition(stack, `EMD-FargateContainer`, {
+  taskDefinition: fargateTaskDefinition,
+  containerName: 'EMD-FargateContainer',
+  image: ecs.ContainerImage.fromAsset(path.resolve(__dirname, '../local-image')),
+  portMappings: [
+      {
+          containerPort: 80,
+          hostPort: 80,
+          protocol: ecs.Protocol.TCP
+      }
+  ],
+  environment: {
+      EMD_VAR: 'option 1',
+  },
+  logging: new ecs.AwsLogDriver({ streamPrefix: "infra" })
+});
+
+// Create Security Group firewall settings
+const ec2SecurityGroup = new ec2.SecurityGroup(stack, 'EMD-EC2SecurityGroup', {
+  vpc,
+  allowAllOutbound: true,
+});
+
+// Allow HTTP traffic from the load balancer
+ec2SecurityGroup.addIngressRule(
+  ec2.Peer.anyIpv4(),
+  ec2.Port.tcp(80),
+  'Allow All HTTP traffic'
+);
+
+const service = new ecs.FargateService(stack, `EMD-ecs-service`, {
+  assignPublicIp: true,
+  cluster: cluster,
+  taskDefinition: fargateTaskDefinition,
+  platformVersion: ecs.FargatePlatformVersion.LATEST,
+  vpcSubnets: {
+      subnets: [
+          vpc.publicSubnets[0],
+          vpc.publicSubnets[1],
+      ]
+  },
+  securityGroups: [ec2SecurityGroup]
+});
+
+// Add HTTP Listener
+const httpListener = alb.addListener(`EMD-HTTPListner`, {
+  port: 80,
+  protocol: ApplicationProtocol.HTTP
+});
+
+// Add listener target 
+httpListener.addTargets('EMD-ECS', {
+  protocol: ApplicationProtocol.HTTP,
+  targets: [service.loadBalancerTarget({
+    containerName: 'EMD-FargateContainer'
+  })],
+
 });
---
+++ local-image/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Favorite dessert:</b> FAVORITE_DESSERT ENV HERE<br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=56789)
