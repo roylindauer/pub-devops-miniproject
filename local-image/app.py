62b57dd7dc577c07a3e3335e06e57dcb30bfad18
Roy Lindauer <1851879+roylindauer@users.noreply.github.com>
2023-03-21 16:31:19 -0700

    Run app on port 80. Print favorite dessert

--- local-image/app.py
+++ local-image/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Favorite dessert:</b> FAVORITE_DESSERT ENV HERE<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+           "<b>Favorite dessert:</b> {favorite_dessert}<br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), favorite_dessert=os.getenv('FAVORITE_DESSERT', 'all of them'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=56789)
+    app.run(host='0.0.0.0', port=80)
